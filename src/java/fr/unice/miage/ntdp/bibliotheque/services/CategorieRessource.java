0a4ba6e20ac045bf35ebcbdf22fb1d72926f6080
Florian <florian.champoussin@gmail.com>
2016-12-07 15:21:51 +0100

    Etapes 1. à 5.

---
+++ src/java/fr/unice/miage/ntdp/bibliotheque/services/CategorieRessource.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.unice.miage.ntdp.bibliotheque.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author florian
+ */
+@Path("catégorie")
+public class CategorieRessource {
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public String hello(){
+        return "La ressource demandée existe!";
+    }
+}
